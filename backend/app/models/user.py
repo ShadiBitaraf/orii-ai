a6fc936ea734b56525eb0ecc490813d0d48777ec
Shadi Bitaraf <shawty@Shadis-MacBook-Air.local>
2025-01-22 17:37:57 -0800

    feature: added models folder and defined a user model

---
+++ backend/app/models/user.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Boolean, Column, Integer, String, DateTime
+from sqlalchemy.sql import func
+from ..database import Base
+import uuid
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    uuid = Column(String, unique=True, index=True, default=lambda: str(uuid.uuid4()))
+    email = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
+    is_active = Column(Boolean, default=True)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    google_token = Column(String, nullable=True)
+    google_refresh_token = Column(String, nullable=True)
+    token_expiry = Column(DateTime, nullable=True)
